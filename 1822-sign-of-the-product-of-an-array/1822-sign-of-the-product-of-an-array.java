957dab57e5fb70a4816fa20d6f48cedee197540d
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 16:26:29 -0400

    Time: 3 ms (6.47%), Space: 42.5 MB (19.35%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,24 +1,16 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 class Solution {
-    
+
 public static int arraySign(int[] nums) {
     
-    	int sign;//holds on the the sign of the product
-    
-        
-        //searches for zeros in array
-    	int[] zero = Arrays.stream(nums).filter(n -> n == 0).toArray();
-    
-        //searches for all negatives in array
-    	nums = Arrays.stream(nums).filter(n -> n < 0).toArray();
-    
-        //checks if there is a zero (returns 0), if not then determines if there is odd or even number of negatives in array
-    	sign = zero.length >= 1 ? 0 : (nums.length % 2 == 0 ? 1 : -1);
-    
+    	int sign = 1;
+    	Arrays.sort(nums);
+    	for(int i = 0; i < nums.length; i++) {
+    			 sign = nums[i] < 0 ? sign * -1 : (nums[i] == 0 ? 0 : sign);
+    	}
 		return sign;
-    	
         
     }
+    	
 }
