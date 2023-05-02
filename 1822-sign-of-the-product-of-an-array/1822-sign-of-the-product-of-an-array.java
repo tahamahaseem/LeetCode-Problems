493370668f050f60dfefa128082a74176f4c7ff8
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 18:15:36 -0400

    Time: 0 ms (100.00%), Space: 42.1 MB (65.02%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -7,9 +7,6 @@ public static int arraySign(int[] nums) {
     	for(int i = 0; i < nums.length; i++) {
             //switches sign upon finding a negative value. If find zero in array, sign becomes zero
     		sign = nums[i] < 0 ? sign * -1 : (nums[i] == 0 ? 0 : sign);
-            if (sign == 0){
-                break;
-            }
     	}
 		return sign;//return the sign
         
