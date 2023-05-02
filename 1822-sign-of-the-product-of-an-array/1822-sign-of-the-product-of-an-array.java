70e012e73b663d0ef8d412e80dec035e3623f1d1
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 18:15:27 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (28.87%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -4,7 +4,6 @@ class Solution {
 public static int arraySign(int[] nums) {
     
     	int sign = 1;// assuming product is positive
-    	Arrays.sort(nums);
     	for(int i = 0; i < nums.length; i++) {
             //switches sign upon finding a negative value. If find zero in array, sign becomes zero
     		sign = nums[i] < 0 ? sign * -1 : (nums[i] == 0 ? 0 : sign);
