3bba23bcd69d94156aa36e73d986d6237ad93d39
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 19:45:07 -0400

    Time: 4 ms (63.53%), Space: 43.9 MB (5.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
 class Solution {
 public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> store = new HashMap();
+		Map<Integer, Integer> store = new HashMap();//look up table with key value pairs
 		int compliment;
 		for (int i = 0; i < nums.length; i++) {
-			store.put(nums[i], i);
+			store.put(nums[i], i);//key holds the integers, and value holds the indecies of those integers
 
 		}
-		for(int i = 0; i<nums.length; i++) {
-			compliment = target - nums[i];
+		for(int i = 0; i<nums.length; i++) {//first number that will sum to target
+			compliment = target - nums[i];//find second number that will sum to target
 			if(store.containsKey(compliment) && store.get(compliment) != i) {
-				nums = new int[]{i, store.get(compliment)};
+				nums = new int[]{i, store.get(compliment)};//makes new array with the integer indecies that add up to target
 			}
 			
 		}
