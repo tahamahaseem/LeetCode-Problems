f39a46a85315b530eb5b0d0067e2cb0a6143d14a
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 19:27:39 -0400

    Time: 4 ms (63.53%), Space: 44 MB (5.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,18 @@
 class Solution {
 public int[] twoSum(int[] nums, int target) {
-    	int[] indecies = new int[2];
-    	int complement;
-    	boolean found = false;
-    	for(int i = 0; i< nums.length; i++) {
-            complement = target - nums[i];
-            for(int j = 0; j < nums.length; j++){
-                if(i != j){
-                    if(nums[i] + nums[j] == target){
-                    	if(i > j) {
-                    		 indecies[1] = i;
-                             indecies[0] = j;
-                    	}else {
-                    		 indecies[0] = i;
-                             indecies[1] = j;
-                    	}
-                    	break;
-                    }
-                }
-                
-            }
-    	}
-		return indecies;
-    }
+		Map<Integer, Integer> store = new HashMap();
+		int compliment;
+		for (int i = 0; i < nums.length; i++) {
+			store.put(nums[i], i);
+
+		}
+		for(int i = 0; i<nums.length; i++) {
+			compliment = target - nums[i];
+			if(store.containsKey(compliment) && store.get(compliment) != i) {
+				nums = new int[]{i, store.get(compliment)};
+			}
+			
+		}
+		return nums;
+	}
 }
