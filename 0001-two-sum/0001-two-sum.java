9f46797a8836357777049e4dda542b376592e4cc
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 18:43:28 -0400

    Time: 217 ms (5.23%), Space: 42.9 MB (51.47%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+public int[] twoSum(int[] nums, int target) {
+    	int[] indecies = new int[2];
+    	int complement;
+    	for(int i = 0; i< nums.length; i++) {
+            complement = target - nums[i];
+            for(int j = 0; j < nums.length; j++){
+                if(i != j){
+                    if(nums[i] + nums[j] == target){
+                    	if(i > j) {
+                    		 indecies[1] = i;
+                             indecies[0] = j;
+                    	}else {
+                    		 indecies[0] = i;
+                             indecies[1] = j;
+                    	}
+                       
+                        
+                    }
+                }
+                
+            }
+    	}
+		return indecies;
+    }
+}
