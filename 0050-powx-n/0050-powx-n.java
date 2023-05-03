a07a8f72e80e8fd933c02fbf5bdec091cd4c40ad
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-03 01:06:17 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (50.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,27 @@
 class Solution {
-    public double myPow(double x, int n) {
-        	return Math.pow(x, n);
-    }
+	 public static double myPow(double x, int n) {
+		double product = x, reserve = 1, power = n;
+
+		if (n == 0) {//if power is 0 then answer will be always 1
+			return 1;
+		} else if (n == 1) {//if power is 1 then answer will be always the base value x
+			return x;
+		}
+
+		if (power < 0) {
+			power *= -1;// converts negative power to positive
+		}
+		while (power > 1) {
+			if (power % 2 == 1) {// checks if the power n is odd
+				power--;// convert odd power to even
+				reserve *= product;// accommodates for odd power
+			}
+
+			// squaring the base and dividing the power by 2 is the same as x^n
+			product *= product;
+			power /= 2;
+		}
+		return n < 0 ? 1 / (product * reserve) : product * reserve;// if odd reserve is not 1.
+
+	}
 }
