1db7871d21853e66f2f66d4371c5befebef0ce44
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-02 23:12:21 -0400

    Time: 92 ms (11.36%), Space: 43.5 MB (29.55%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,26 +1,32 @@
 class Solution {
     
 	public static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        
 		List<List<Integer>> nums = new ArrayList<>(Arrays.asList(new ArrayList<>(), new ArrayList<>()));
-		int length = nums1.length >= nums2.length ? nums1.length : nums2.length;
+		
+        //gets the highest length from the two arrays
+        int length = nums1.length >= nums2.length ? nums1.length : nums2.length;
+        
+        //sort arrays for Arrays.binarySearch
 		Arrays.sort(nums1);
 	    Arrays.sort(nums2);
 		
+        //iterates largest array number of times
 		for (int i = 0; i < length; i++) {
 
-			if (i < nums1.length) {
+			if (i < nums1.length) {//checks if iteration in not beyond the array size
 				if (Arrays.binarySearch(nums2, nums1[i]) <= -1) {
-					if (!(nums.get(0).contains(nums1[i]))) {
-						nums.get(0).add(nums1[i]);
+					if (!(nums.get(0).contains(nums1[i]))) {//checks if no duplicate
+						nums.get(0).add(nums1[i]);//adds the unique integer into the list
 					}
 
 				}
 			}
 
-			if (i < nums2.length) {
+			if (i < nums2.length) {//checks if iteration in not beyond the array size
 				if (Arrays.binarySearch(nums1, nums2[i]) <= -1) {
-					if (!(nums.get(1).contains(nums2[i]))) {
-						nums.get(1).add(nums2[i]);
+					if (!(nums.get(1).contains(nums2[i]))) {//checks if no duplicate
+						nums.get(1).add(nums2[i]);//adds the unique integer into the list
 					}
 				}
 			}
