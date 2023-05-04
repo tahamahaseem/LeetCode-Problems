68c3c35e826f083d8b97a2bcf50311e4262183f0
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-04 18:40:00 -0400

    Time: 4 ms (23.32%), Space: 43.3 MB (46.61%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,27 @@
 class Solution {
     public static int pivotIndex(int[] nums) {
 
-		int arraySum = IntStream.of(nums).sum();
-		int leftSum = 0, rightSum = arraySum;
-		int pivot, tempPivot = 0;
+		int arraySum = IntStream.of(nums).sum();//gets the sum of the array
+        
+		int leftSum = 0, rightSum = arraySum;//holds the left side and right side sums
+		int pivot, tempPivot = 0;//pivot variables
+        
 		for (int i = 0; i < nums.length; i++) {
-			pivot = nums[i];
-			rightSum -= pivot;
-			leftSum += tempPivot;
+			pivot = nums[i];//trying all pivots from left to right
+			
+            //right sum increases and left sum decrease
+            rightSum -= pivot;
+            leftSum += tempPivot;
+            
+            //checks if the two sums are equal
 			if (leftSum == rightSum) {
-				return i;
+				return i;//returns the index of the correct pivot
 			}
-			tempPivot = pivot;
+            
+			tempPivot = pivot;//holds previous pivot to subtract from left side
 
 		}
-		return -1;
+		return -1;//returns -1 if no index found
 
 	}
 }
