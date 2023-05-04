75a7b356a78ede5d67bf7e63e53507a214698c7d
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-03 22:59:16 -0400

    Time: 2 ms (99.00%), Space: 42.3 MB (91.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -12,33 +12,32 @@ class Solution {
    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		int carry = 0;
 		int sum = 0;
-		ListNode head = new ListNode(0);
-		ListNode current = head;
+		ListNode head = new ListNode(0);//starting node used as a dummy
+		ListNode current = head;//pointer to current node which is the head
 		
-		while (l1 != null || l2 != null) {
+		while (l1 != null || l2 != null) {//iterates until went through both l1 and l2 nodes
 
-			if (l1 != null) {
+			if (l1 != null) {//iterates until all nodes of l1 are searched
 				sum += l1.val;
 				l1 = l1.next;
 			}
-			if (l2 != null) {
+			if (l2 != null) {//iterates until all nodes of l2 are searched
 				sum += l2.val;
 				l2 = l2.next;
 			}
-			carry = sum / 10;
-			sum %= 10;
-			System.err.println(sum);
-			
-			current.next = new ListNode(sum);
-			current = current.next;
-			sum = carry;
+			carry = sum / 10;//finds if carry exists
+			sum %= 10;//gets sum without the carry (0-9 integers)
+		
+			current.next = new ListNode(sum);//creates the next node with the sum value
+			current = current.next;//moves to the next node
+			sum = carry;//resets the sum so that it only starts with the value of the carry
 		}
-		if(carry > 0) {
+		if(carry > 0) {//accomodates for the LAST carry node
 			current.next = new ListNode(carry);
 		}
 
 
-		return head.next;
+		return head.next;//passes the dummy node
 
 	}
 }
