a80f0f0ffe47b3e2f741de49442e69e2cf9e70eb
tahamahaseem <52297139+tahamahaseem@users.noreply.github.com>
2023-05-04 07:28:53 -0400

    Time: 16 ms (7.95%), Space: 45.9 MB (6.40%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -2,12 +2,12 @@ class Solution {
 public static String numberToWords(int num) {
 		Map<Integer, String> store = new HashMap<>();
 
-		// all possible key value pairs for english word representation of integers
-		
-        if(num == 0) {
+		// if the num is 0, then return Zero
+		if (num == 0) {
 			return "Zero";
 		}
-    
+
+		// all possible key value pairs for english word representation of integers
 		store.put(1, "One");
 		store.put(2, "Two");
 		store.put(3, "Three");
@@ -40,76 +40,94 @@ public static String numberToWords(int num) {
 		store.put(1000000, "Million");
 		store.put(1000000000, "Billion");
 
-		String numString = Integer.toString(num);
+		String numString = Integer.toString(num);// converts num to a string to perform substring operation
 
-		int numLength = numString.length();
+		int numLength = numString.length();// holds the length of the num to determine decimal places like tens, hundredths
+											// etc
 		String hundreds = null, thousands = null, millions = null, billions = null;
 
-		if (numLength > 9) {
+		// This block of code divides the number into 4 decimal places (billions, millions, thousands and hundredth place)
+		if (numLength > 9) {// checks if num has billions place
+			
 			billions = numString.substring(0, numLength - 9);
 			millions = numString.substring(billions.length(), numLength - 6);
 			thousands = numString.substring(millions.length() + 1, numLength - 3);
 			hundreds = numString.substring(numLength - 3);
 
-		} else if (numLength > 6) {
+		} else if (numLength > 6) {// checks if num has millions place
+			
 			millions = numString.substring(0, numLength - 6);
 			thousands = numString.substring(millions.length(), numLength - 3);
 			hundreds = numString.substring(numLength - 3);
-		} else if (numLength > 3) {
+
+		} else if (numLength > 3) {// checks if num has thousands place
+			
 			thousands = numString.substring(0, numLength - 3);
 			hundreds = numString.substring(numLength - 3);
-		} else {
+
+		} else {// num is less than thousands place
 			hundreds = numString.substring(0);
 		}
 
-		String[] placings = new String[] { billions, millions, thousands, hundreds };
+		String[] placings = new String[] { billions, millions, thousands, hundreds };// decimal places are put into an array
 
 		int remainder = 0, quotient;
-		String temp = "", englishRep = "";
+		String temp = "", englishRep = "";// temp holds temporarily parts of the English representations, englishRep will
+											// hold the final sentence
 		for (int i = 0; i < placings.length; i++) {
 
-			if (placings[i] != null &&  Integer.parseInt(placings[i]) != 0) {
+			if (placings[i] != null && Integer.parseInt(placings[i]) != 0) {// checks if the current decimal place in array
+																			// is not null and not zero.
 
 				int placingInt = Integer.parseInt(placings[i]);
 
-				do {
+				do {//loops until placing has been constructed into English
 					quotient = placingInt;
-					if (placingInt <= 20) {
-						temp += store.get(placingInt) + " ";
-						break;
-					} else if (placingInt < 100) {
-
-						remainder = quotient % 10;
-						quotient /= 10;
-						temp += store.get(quotient * 10) + " "
-								+ (remainder == 0 ? "" : store.get(remainder) + " ");
-						break;
-					} else {
-						remainder = quotient % 100;
-						quotient /= 100;
-						temp = store.get(quotient) + " " + store.get(100) + " ";
-						placingInt = remainder;
+					if (placingInt <= 20) {//first 20 numbers in the number system have unique names so therefore they require a special case
 						
+						temp += store.get(placingInt) + " ";//gets the exact value from the store
+						break;//we are done breaks the loop and goes to the next placing
+						
+					} else if (placingInt < 100) {//the first 99 numbers in the number system repeat again in different decimal places, therefore they need their own case
+
+						remainder = quotient % 10;//returns the integer ones place 
+						quotient /= 10;//returns the integer of the hundredth and tenth place 
+						
+						//translates the numerical values to English using the store. Note that it checks if remainder is zero so that it does not print a Zero/null for the tenths and ones place. For example for 50, "Fifty Zero" or "Fifty null" does not make sense.
+						temp += store.get(quotient * 10) + " " + (remainder == 0 ? "" : store.get(remainder) + " ");
+						
+						break;//we are done breaks the loop and goes to the next placing
+						
+					} else {//if the placing portion is greater than 99
+						
+						remainder = quotient % 100;//returns the integer of the tenth and ones place 
+						quotient /= 100;//returns the integer of the hundredth place 
+						
+						//translates the numerical values to English using the store. Only returns the hundredth place
+						temp = store.get(quotient) + " " + store.get(100) + " ";
+						
+						//sets the tenth and ones to placingInt such that the loop, loops back to decompose that rest of the remaining tenth and ones place
+						placingInt = remainder;
 
 					}
 
 				} while (remainder != 0);
 
-				
-				if (i == 0) {// in billions
+				if (i == 0) {//adds billion to the current placing aka the temp string
 					temp += store.get(1000000000) + " ";
-				} else if (i == 1) {// in millions
+				} else if (i == 1) {//adds million to the current placing aka the temp string
 					temp += store.get(1000000) + " ";
-				} else if (i == 2) {// in thousands
+				} else if (i == 2) {//adds thousand to the current placing aka the temp string
 					temp += store.get(1000) + " ";
 				}
 
-				englishRep += temp;
-				temp = "";
+				englishRep += temp;//adds it to the final sentence variable
+				temp = "";//resets temp for the next decimal place
 			}
 
 		}
-
+		
+		//trims any empty space at the front and back of the sentence and returns final sentence
 		return englishRep.trim();
 
 	}
