f0a918b2feb5102625d78b5e2ed73cc7cc0702fd
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2024-09-05 04:51:59 -0400

    Time: 198 ms (29.02%), Space: 71.2 MB (37.02%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.ts
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.ts
@@ -1,28 +1,28 @@
 function construct2DArray(original: number[], m: number, n: number): number[][] {
-    // Calculate the length of the original array
+    // Calculate the length of the original array.
     let length: number = original.length;
     
-    // Check if it's possible to form a 2D array with m rows and n columns
+    // Check if it's possible to form a 2D array with m rows and n columns.
     if (n * m !== length) {
-        return []; // Return an empty array if not possible
+        return []; // Return an empty array if not possible.
     } else {
         // Initialize variables
-        let currentArray: number = -1; // Index for tracking the current row in result
-        let nextRowIndex: number = 0;  // Index for tracking the start of the next row in original array
-        let result: number[][] = [];   // Initialize the result array
+        let currentArray: number = -1; // Index for tracking the current row in result. In the beginning there is no array intialized.
+        let nextRowIndex: number = 0;  // Index for tracking the start of the next row in original array.
+        let result: number[][] = [];   // Initialize the result array.
 
-        // Iterate over each element in the original array
+        // Iterate over each element in the original array.
         for (let i: number = 0; i < length; i++) {
-            // Check if it's time to start a new row
+            // Check if it's time to start a new row.
             if (i === nextRowIndex) {
-                // Start a new row and push it to the result
+                // Start a new row and push it to the result.
                 result.push([original[nextRowIndex]]);
-                // Move to the start index of the next row
+                // Move to the start index of the next row.
                 nextRowIndex += n;
-                // Move to the next row index
+                // Move to the next row index.
                 currentArray++;
             } else {
-                // Add the element to the current row if it exists
+                // Add the element to the current row if it exists.
                 if (Array.isArray(result[currentArray])) {
                     result[currentArray].push(original[i]);
                 }
