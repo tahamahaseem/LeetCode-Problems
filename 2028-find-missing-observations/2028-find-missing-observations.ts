5c28d55161c562f92f0ae1656f9d26462c713ea4
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2024-09-05 02:00:39 -0400

    Time: 262 ms (100.00%), Space: 72.9 MB (50.00%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.ts
@@ -0,0 +1,28 @@
+function missingRolls(rolls: number[], mean: number, n: number): number[] {
+    let totalRolls: number = rolls.length + n;
+    let totalKnownValues: number = rolls.reduce((sum, i) => sum + i, 0);
+    let totalMissingValues: number = mean*(totalRolls) - totalKnownValues;
+    if(n == 0){
+        return [];
+    }
+    else if (n == 1){
+        return [totalMissingValues]
+    }
+    else {
+        let quotient: number;
+        let leftOverMissingValues: number = totalMissingValues
+        let result: number[] = [];
+        for(let observation: number = n ; observation > 0 ; observation--){
+             quotient = Math.ceil(leftOverMissingValues / observation);
+            if (quotient <= 6 && quotient >= 1) {
+                result.push(quotient);
+            } else {
+                return [];
+                
+            }
+            leftOverMissingValues -= quotient;
+        }
+        return result;
+    }
+    
+};
