3b481b8d495d89b6bd47c0777cf73ff81e2bb658
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2024-09-05 04:51:01 -0400

    Time: 178 ms (74.93%), Space: 70.8 MB (38.30%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.ts
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.ts
@@ -1,23 +1,34 @@
 function construct2DArray(original: number[], m: number, n: number): number[][] {
+    // Calculate the length of the original array
     let length: number = original.length;
-    if(n * m !== length){
-        return []
-    }else{
-        let currentArray: number = -1;
-        let nextRowIndex: number = 0;
-        let result: number[][] = [];
-        for(let i: number = 0; i < length ; i++){
-            if(i === nextRowIndex){
+    
+    // Check if it's possible to form a 2D array with m rows and n columns
+    if (n * m !== length) {
+        return []; // Return an empty array if not possible
+    } else {
+        // Initialize variables
+        let currentArray: number = -1; // Index for tracking the current row in result
+        let nextRowIndex: number = 0;  // Index for tracking the start of the next row in original array
+        let result: number[][] = [];   // Initialize the result array
+
+        // Iterate over each element in the original array
+        for (let i: number = 0; i < length; i++) {
+            // Check if it's time to start a new row
+            if (i === nextRowIndex) {
+                // Start a new row and push it to the result
                 result.push([original[nextRowIndex]]);
+                // Move to the start index of the next row
                 nextRowIndex += n;
+                // Move to the next row index
                 currentArray++;
             } else {
-                if(Array.isArray(result[currentArray])){
+                // Add the element to the current row if it exists
+                if (Array.isArray(result[currentArray])) {
                     result[currentArray].push(original[i]);
                 }
             }
-           
         }
+        // Return the constructed 2D array
         return result;
     }
-};
+}
