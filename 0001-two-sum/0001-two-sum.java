f8e58dfb6ea9f3d6fc1c97ba2c7d47d5f5d8dbf0
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2025-06-04 11:24:30 -0400

    Time: 2 ms (98.81%), Space: 45.2 MB (18.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,19 @@
 class Solution {
-public int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> store = new HashMap();//look up table with key value pairs
-		int compliment;
-		for (int i = 0; i < nums.length; i++) {
-			store.put(nums[i], i);//key holds the integers, and value holds the indecies of those integers
+    public int[] twoSum(int[] nums, int target) {
 
-		}
-		for(int i = 0; i<nums.length; i++) {//first number that will sum to target
-			compliment = target - nums[i];//find second number that will sum to target
-			if(store.containsKey(compliment) && store.get(compliment) != i) {
-				nums = new int[]{i, store.get(compliment)};//makes new array with the integer indecies that add up to target
-			}
-			
-		}
-		return nums;
-	}
+            Map<Integer, Integer> integerMap = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+
+                //filter out values greater than target
+                int difference = target - nums[i];
+                if (integerMap.containsKey(difference))
+                    return new int[]{integerMap.get(difference), i};
+                    
+                //store number into map
+                integerMap.put(nums[i], i);
+            }
+            //return -1 indecies if no answer exists
+            return new int[]{-1, -1};
+        }
 }
