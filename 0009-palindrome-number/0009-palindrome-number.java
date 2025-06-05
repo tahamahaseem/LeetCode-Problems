b4610fc90c0e574f8e68b1214d7319c2a8b55a5e
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2025-06-05 07:40:22 -0400

    Time: 22 ms (5.75%), Space: 44.8 MB (5.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0)
+        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
         else if (x <= 9)
             return true;
@@ -8,16 +8,14 @@ class Solution {
         boolean isPalindrome = true;
         String[] nums = Integer.toString(x).split("");
         int i = 0;
-        int j = nums.length - 1;
 
 
-        while (i < j) {
-            if (!nums[i].equals(nums[j])) {
+        while (i < nums.length - 1 - i) {
+            if (!nums[i].equals(nums[nums.length - 1 - i])) {
                 isPalindrome = false;
                 break;
             }
             i++;
-            j--;
         }
 
         return isPalindrome;
