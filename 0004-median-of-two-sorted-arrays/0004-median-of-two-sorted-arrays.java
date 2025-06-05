c480a0be53001032884638b24cb276e70d9a9758
Tahama Haseem <52297139+tahamahaseem@users.noreply.github.com>
2025-06-05 07:18:46 -0400

    Time: 2 ms (55.01%), Space: 46.2 MB (44.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,12 +1,21 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //get totalLength of merged array for calculations
         int totalLength = nums1.length + nums2.length;
+
+        /*determines whether the median will need to be calculated by adding
+        the two middle values and finding average or just by taking the single
+        middle value in array based on the totalLength */
         boolean medianIsInteger = (totalLength) % 2 != 0;
+
         int[] mergedArray = new int[totalLength];
+
+        //pointer for final array (i), and the left and right arrays given to merge
+        int i = 0;
         int leftPointer = 0;
         int rightPointer = 0;
-        int i = 0;
 
+        //merges and sorts the final array using the left and right arrays using merge sort merge algorithm
         while (leftPointer < nums1.length && rightPointer < nums2.length) {
             if (nums1[leftPointer] <= nums2[rightPointer]) {
                 mergedArray[i] = nums1[leftPointer];
@@ -18,24 +27,27 @@ class Solution {
             i++;
         }
 
+        //since given arrays are already sorted, just concat the rest of the left array since right array is already all in
         while (leftPointer < nums1.length) {
             mergedArray[i] = nums1[leftPointer];
             leftPointer++;
             i++;
         }
-
+        //since given arrays are already sorted, just concat the rest of the right array since left array is already all in
         while (rightPointer < nums2.length) {
             mergedArray[i] = nums2[rightPointer];
             rightPointer++;
             i++;
         }
 
+        //find the first or only median index
         int medianIndex = (int) Math.ceil((double) mergedArray.length / 2) - 1;
 
+        //based on array size calculate median two different ways
         if (medianIsInteger) {
             return mergedArray[medianIndex];
         } else {
-            return (double)(mergedArray[medianIndex] + mergedArray[medianIndex + 1]) / 2;
+            return (double) (mergedArray[medianIndex] + mergedArray[medianIndex + 1]) / 2;
         }
     }
 }
